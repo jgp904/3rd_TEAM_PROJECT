a9989bdbc768a18ea9d253d843547d18c42e2f2c
ssknu1 <rlarjsdn94@gmail.com>
2023-07-12 09:30:51 +0900

    ..

--- 3rd_TEAM_PROJECT/Models/Process/MProcess.cs
+++ 3rd_TEAM_PROJECT/Models/Process/MProcess.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _3rd_TEAM_PROJECT.Models.Process
 {
-    [Table("T1_MProcess")]
-    public class MProcess
 	[Table("T1_MProcess")]
 	public class MProcess
 	{
